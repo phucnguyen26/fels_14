b763b134b8de1243a4e82c46a71d7b97d30d6e2e
anhhung1303 <anhhung1303@gmail.com>
2014-10-11 15:29:57 +0700

    Sign in/out

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,32 @@
+module SessionsHelper
+  
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.not_validate_password = true
+    user.update_attributes(remember_token: User.digest(remember_token))
+    user.not_validate_password = false
+    self.current_user = user
+  end
+  
+  def user_signed_in?
+    !current_user.nil?
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+  
+  def sign_out
+    user.not_validate_password = true
+    current_user.update_attributes(remember_token: User.digest(User.new_remember_token))
+    user.not_validate_password = false
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,27 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase}
+  before_create :create_remember_token
+  
   validates_presence_of :name, length: {minimum: 1, maximum: 60}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, length: {minimum: 6}
+  
+  attr_accessor :not_validate_password
+  validates :password,  presence: true, length: { minimum: 6 }, unless: :not_validate_password
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+  
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
